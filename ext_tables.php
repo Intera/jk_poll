77e95d5b9d1c588bf5ae1f08b18d9eabc097a4dd
Alexander Stehlik <alexander.stehlik@googlemail.com>
2013-09-05 17:02:16 +0200

    [!!!][TASK] Changed TypoScript inclusion method
    The global inclusion of TypoScript with the ext_typoscript_setup.txt
    file was replaced with a call to t3lib_extMgm::addStaticFile().
    
    The file was moved to Configuration/TypoScript/setup.txt
    
    Warning! Template needs now to be included manually.

--- ext_tables.php
+++ ext_tables.php
@@ -40,7 +40,7 @@ $TCA["tt_content"]["types"]["list"]["subtypes_excludelist"][$_EXTKEY."_pi1"]="la
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='pi_flexform';
 
 t3lib_extMgm::addPlugin(Array("LLL:EXT:jk_poll/locallang_db.xml:tt_content.list_type_pi1", $_EXTKEY."_pi1"),"list_type");
-t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","Poll");
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Poll');
 t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:jk_poll/flexform_ds.xml');
 
 if (TYPO3_MODE=="BE")	$TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_jkpoll_pi1_wizicon"] = t3lib_extMgm::extPath($_EXTKEY)."pi1/class.tx_jkpoll_pi1_wizicon.php";
