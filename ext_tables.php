1736fa24d5f5d8daa0858749b682d010bfbc33e0
Alexander Stehlik <alexander.stehlik@googlemail.com>
2013-10-17 14:20:35 +0200

    [TASK] Updated TYPO3 API / code style in ext_*.php files
    The TYPO3 API usage was updated to TYPO3 6.x and the code style was
    improved in ext_localconf.php and ext_tables.php.

--- ext_tables.php
+++ ext_tables.php
@@ -1,16 +1,13 @@
 <?php
-if (!defined ("TYPO3_MODE")) 	die ("Access denied.");
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
 
-t3lib_extMgm::allowTableOnStandardPages("tx_jkpoll_poll");
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_jkpoll_poll');
 
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
 
-t3lib_div::loadTCA("tt_content");
-$TCA["tt_content"]["types"]["list"]["subtypes_excludelist"][$_EXTKEY."_pi1"]="layout,select_key";
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='pi_flexform';
-
-t3lib_extMgm::addPlugin(Array("LLL:EXT:jk_poll/locallang_db.xml:tt_content.list_type_pi1", $_EXTKEY."_pi1"),"list_type");
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Poll');
-t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:jk_poll/flexform_ds.xml');
-
-if (TYPO3_MODE=="BE")	$TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_jkpoll_pi1_wizicon"] = t3lib_extMgm::extPath($_EXTKEY)."pi1/class.tx_jkpoll_pi1_wizicon.php";
-?>
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array('LLL:EXT:jk_poll/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY . '_pi1'), 'list_type');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Poll');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:jk_poll/flexform_ds.xml');
